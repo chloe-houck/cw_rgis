cb4e007da64949c7db3147afc40a077e93528335
chloe-houck <chouck03@gmail.com>
2026-08-20 22:59:21 -0400

    First report done.

---
+++ code/script01_data_structure.R
@@ -0,0 +1,12 @@
+a <- 1.2
+b <- "a"
+va <- c(1.2, 3, 4.6)
+mean(va)
+vb <- c("1", "2", "3")
+vc <- c(1.3, 4.2, 5.6)
+vd <- c(10, 3, 2)
+ma <- cbind(vc, vd)
+mb <- rbind(vc, vd)
+vz <- c("a", "b", "c")
+vf <- factor(vz)
+df0 <- data.frame("Letter" = vz, "Numeric" = va)
