d545f951e059e297026b82c419d4ecc2a0440f98
chloe-houck <chouck03@gmail.com>
2026-09-10 15:01:10 -0400

    Section 1 of the first exam done.

--- code/exam_i.R
+++ code/exam_i.R
@@ -10,38 +10,49 @@ df_mtcars <- as_tibble(mtcars,
 # 1. Filter rows where number of cylinders (`cyl`) is 6
 # Write code to create a new data frame that only includes rows where `cyl == 6`.
 # Assign to: `mtcars_cyl6`
+mtcars_cyl6 <- filter(df_mtcars, cyl == 6)
 
 # 2. Filter rows where number of gears (`gear`) is either 3 or 5
 # Use `%in%` to filter the `df_mtcars` dataset for these two `gear` values.
 # Assign to: `mtcars_g35`
+mtcars_g35 <- filter(df_mtcars, gear == 3 | gear == 5)
 
 # 3. Filter rows where miles per gallon (`mpg`) is greater than 25
 # Create a subset of `df_mtcars` where `mpg > 25`.
 # Assign to: `mtcars_mpg25`
+mtcars_mpg25 <- filter(df_mtcars, mpg > 25)
 
 # 4. Filter rows where weight (`wt`) is less than 2.5 AND number of cylinders (`cyl`) is 4
 # Combine logical conditions using `&`.
 # Assign to: `mtcars_light_cyl4`
+mtcars_light_cyl4 <- filter(df_mtcars, wt < 2.5) %>% 
+  filter(cyl == 4)
 
 # 5. Sort `df_mtcars` by horsepower (`hp`) in ascending order
 # Use `arrange()` to sort the data.
 # Assign to: `mtcars_hp`
+mtcars_hp <- arrange(df_mtcars, hp)
 
 # 6. Sort `df_mtcars` by quarter mile time (`qsec`) in descending order
 # Use `desc()` inside `arrange()`.
 # Assign to: `mtcars_qsec`
+mtcars_qsec <- arrange(df_mtcars, desc(qsec))
 
 # 7. Exclude the `drat` column
 # Use `select()` with `-` (minus sign) to remove the column.
 # Assign to: `mtcars_no_drat`
+mtcars_no_drat <- select(df_mtcars, -(drat))
 
 # 8. Add a new column `ptw` that equals the ratio of horsepower (`hp`) to weight (`wt`) (`hp / wt`)
 # Use `mutate()` to add the new column.
 # Assign to: `mtcars_with_ptw`
+mtcars_with_ptw <- mutate(df_mtcars, "ptw" = hp/wt)
 
 # 9. Identify the car `model` with the highest `ptw` among cars with six cylinders (`cyl == 6`).
 # Hint: Use `mtcars_with_ptw` and a chain of `filter()` and `arrange()` with `%>%`.
-# Write the car model here: YOUR ANSWER
+# Write the car model here: Ferrari Dino
+mtcars_ptw_model <- filter(mtcars_with_ptw, cyl == 6) %>% 
+  arrange(desc(ptw))
 
 # 10. Group by number of gears (`gear`) and summarize minimum & maximum values of `mpg`
 # Use `group_by()` and `summarize()` to calculate minimum & maximum values of `mpg` within each group.
@@ -49,6 +60,10 @@ df_mtcars <- as_tibble(mtcars,
 # Function `min()` returns the minimum value in a vector.
 # Function `max()` returns the maximum value in a vector.
 # Assign to: `mtcars_mpg_by_gear`
+mtcars_mpg_by_gear <- group_by(df_mtcars, gear) %>% 
+  mutate("min" = min(mpg)) %>% 
+  mutate("max" = max(mpg)) %>% 
+  ungroup()
 
 # ggplot ------------------------------------------------------------------
 
